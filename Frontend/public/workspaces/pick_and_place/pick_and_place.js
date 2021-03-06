3cf75f5e09eedb7e867250eea776c0966bed2c72
Jacob Slack <jake@ideamagnate.com>
2021-03-05 16:17:42 -0800

    make unity publish in radians - all ros stuff should be in radians

--- Frontend/public/workspaces/pick_and_place/pick_and_place.js
+++ Frontend/public/workspaces/pick_and_place/pick_and_place.js
@@ -1,22 +1,28 @@
 async function start() {
     
-    var scene_data = await api.getSceneData();
-    
-    positions = {
-        joint_00: scene_data.joint_00 + (Math.random() * 2 -1) / 100,
-        joint_01: scene_data.joint_01 + (Math.random() * 2 -1) / 100,
-        joint_02: scene_data.joint_02 + (Math.random() * 2 -1) / 100,
-        joint_03: scene_data.joint_03 + (Math.random() * 2 -1) / 100,
-        joint_04: scene_data.joint_04 + (Math.random() * 2 -1) / 100,
-        joint_05: scene_data.joint_05 + (Math.random() * 2 -1) / 100
+    while (true) {
+        var scene_data = await api.getSceneData();
+        
+        positions = {
+            joint_00: scene_data.joint_00 + (Math.random() * 2 -1) / 100,
+            joint_01: scene_data.joint_01 + (Math.random() * 2 -1) / 100,
+            joint_02: scene_data.joint_02 + (Math.random() * 2 -1) / 100,
+            joint_03: scene_data.joint_03 + (Math.random() * 2 -1) / 100,
+            joint_04: scene_data.joint_04 + (Math.random() * 2 -1) / 100,
+            joint_05: scene_data.joint_05 + (Math.random() * 2 -1) / 100
+        }
+        
+        log(JSON.stringify(positions));
+        
+        var position_cmd = {
+                cmd_type: 4 /* positions */,
+                positions: positions
+            };
+        
+        await api.doMove({ cmd: position_cmd });
+        await api.waitNextResult();
     }
-    
-    var position_cmd = {
-            cmd_type: 4 /* positions */,
-            positions: positions
-        };
-    
-    await api.doMove({ cmd: position_cmd });
+        
     /*
     api.clearResult();
     log("Starting...");
