6c4f76c4dcba7b1b0ee97f206ca44deef606d4f6
Jacob Slack <jake@ideamagnate.com>
2021-03-06 17:15:20 -0800

    event-driven wait for result w/ web socket. This makes moving more response

--- Frontend/public/workspaces/pick_and_place/pick_and_place.js
+++ Frontend/public/workspaces/pick_and_place/pick_and_place.js
@@ -1,30 +1,4 @@
 async function start() {
-    
-    while (true) {
-        var scene_data = await api.getSceneData();
-        
-        positions = {
-            joint_00: scene_data.joint_00 + (Math.random() * 2 -1) / 100,
-            joint_01: scene_data.joint_01 + (Math.random() * 2 -1) / 100,
-            joint_02: scene_data.joint_02 + (Math.random() * 2 -1) / 100,
-            joint_03: scene_data.joint_03 + (Math.random() * 2 -1) / 100,
-            joint_04: scene_data.joint_04 + (Math.random() * 2 -1) / 100,
-            joint_05: scene_data.joint_05 + (Math.random() * 2 -1) / 100
-        }
-        
-        log(JSON.stringify(positions));
-        
-        var position_cmd = {
-                cmd_type: 4 /* positions */,
-                positions: positions
-            };
-        
-        await api.doMove({ cmd: position_cmd });
-        await api.waitNextResult();
-    }
-        
-    /*
-    api.clearResult();
     log("Starting...");
     
     log("Getting Latest Scene Data...");
@@ -43,7 +17,6 @@ async function start() {
 
     log("Opening gripper...");
     await api.doOpenGripper();
-    await api.waitNextResult();
     log("Done");
 
     log("Planning lowering of arm over object...");
@@ -53,7 +26,6 @@ async function start() {
 
     log("Closing gripper...");
     await api.doCloseGripper();
-    await api.waitNextResult();
     log("Done");
 
     log("Executing place trajectory...")
@@ -66,6 +38,5 @@ async function start() {
 
     log("Opening gripper...");
     await api.doOpenGripper();
-    await api.waitNextResult();
-    log("Done");*/
+    log("Done");
 }
--- Frontend/sockets.js
+++ Frontend/sockets.js
@@ -1,4 +1,5 @@
 var net = require('net');
+const {_, subscribers} = require('./subscriber');
 
 // TODO: in production, we would want to replace this with reading from wherever docker logging driver
 // sends this.
@@ -20,9 +21,22 @@ function createRosLogHandler(socket) {
     };
 }
 
+function createTopicSubscriberHandler(socket) {
+    return function(topic) {
+        socket.data_cb = data => {
+            socket.emit(topic, data)
+        };
+        subscribers[topic].addDataCallback(socket.data_cb);
+        socket.on('disconnect', function() {
+            subscribers[topic].removeDataCallback(socket.data_cb);
+        });
+    };
+}
+
 function addHandlers(io) {
     io.on('connection', function(socket) {
         socket.on('ros_log', createRosLogHandler(socket));
+        socket.on('subscribe', createTopicSubscriberHandler(socket));
     });
 }
 
