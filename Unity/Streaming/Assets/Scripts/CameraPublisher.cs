30b2b610431f8a135c34e2e769ed58d15ae900e9
Jacob Slack <jake@ideamagnate.com>
2021-03-16 17:39:12 -0700

    Add overhead camera stream & topic

---
+++ Unity/Streaming/Assets/Scripts/CameraPublisher.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraPublisher : MonoBehaviour
+{
+    private ROSConnection ros;
+    public Camera camera;
+    public string topic;
+    private RenderTexture renderTexture;
+
+    private int renderTextureHeight = 200;
+    private int renderTextureWidth = 200;
+    private int renderTextureDepth = 0;
+    private RenderTextureFormat renderTextureFormat = RenderTextureFormat.ARGB32;
+    private byte[] rawBytes = new byte[160000];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ros = ROSConnection.instance;
+        renderTexture = new RenderTexture(renderTextureWidth, renderTextureHeight, renderTextureDepth, renderTextureFormat);
+        camera.targetTexture = renderTexture;
+
+        StartCoroutine(DoPublish());
+    }
+
+    private IEnumerator DoPublish()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(ReadPixels());
+            Publish();
+            yield return new WaitForSeconds(1.0f); // 1Hz
+        }
+    }
+
+
+    private IEnumerator ReadPixels()
+    {
+        var request = AsyncGPUReadback.Request(renderTexture, 0);
+
+        while (!request.done)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        request.GetData<byte>().CopyTo(rawBytes);
+    }
+
+    private void Publish()
+    {
+        var frame = new RosMessageTypes.NiryoMoveit.Camera();
+        frame.frame.height = (uint) renderTextureHeight;
+        frame.frame.width = (uint) renderTextureWidth;
+        frame.frame.data = rawBytes;
+        ros.Send(topic, frame);
+    }
+}
