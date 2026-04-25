a4094f773b45f46ebb15bcb5d653d5f7aca586ef
frenb <benjamin.frenkel@gmail.com>
2026-04-25 16:50:05 -0700

    mintor cleanup

---
+++ Frontend/public/workspaces/python/environments/__init__.py
@@ -0,0 +1,3 @@
+from .robotaxi_env import RobotaxiEnv
+
+__all__ = ['RobotaxiEnv'] 
---
+++ Frontend/public/workspaces/python/environments/courses/__init__.py
@@ -0,0 +1,4 @@
+from .donut_course import DonutCourse
+from .simple_course import SimpleCourse
+
+__all__ = ['DonutCourse', 'SimpleCourse'] 
